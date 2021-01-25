b10821f80cb12074936bc1b821e0f6a28e2ba43d
Julia <57764881+julia-999@users.noreply.github.com>
2021-01-24 20:02:23 -0500

    Feature/login (#18)
    * started login
    
    * google sign in
    
    replaced text boxes with a button that will sign you in with google once I figure out firebase
    
    * google sign in added!
    
    the google sign in pops up now but i'm not sure what to do with the data yet
    
    * fixed API key and other small edits
    
    * made other ids hidden
    
    * Delete .eslintcache
    
    * resolve error
    
    Co-authored-by: jhwan7 <46652246+jhwan7@users.noreply.github.com>

--- src/Containers/Pages/Login/Login.js
+++ src/Containers/Pages/Login/Login.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, {useState} from 'react';
+import styles from './Login.module.css';
+import logo from "./googleicon.png";
+import firebase from '../../../firebase'
+  
+//   import { config } from "./test-credentials";
 
 class Login extends React.Component {
-  render() {
-    return <div>Login</div>;
-  }
+
+    // not used for now
+    state = {
+        signedIn: false,
+        user: null,
+    }
+
+    googleSignIn () {
+        const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(googleAuthProvider);
+        console.log("Google Sign In Page");
+    }
+
+    helpNeeded () {
+        //TODO: go to the help page (if there is one)
+    }
+
+    render () {
+        return(
+            <div className={styles.loginComponent}>
+                <h1 className={styles.loginTitle}>Login</h1>
+                <p className={styles.loginParagraph}>Welcome Back</p>
+                <p className={styles.loginParagraph}>Sign in to continue</p>
+                <img className={styles.loginLogo} src={logo} alt="Google Logo"></img>
+                <button className={styles.loginButton} onClick={this.googleSignIn}>Sign in with Google</button>
+                <p className={styles.loginParagraphOrange} onClick={this.helpNeeded}>Need Help?</p>
+            </div>
+        );
+    }
 }
 
 export default Login;
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH,
+    projectId: process.env.REACT_APP_PROJ_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESS_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
+  };
+firebase.initializeApp(firebaseConfig);
+export default firebase;
+
+// you have to npm install firebase --dev and npm install @react-native-firebase/auth
